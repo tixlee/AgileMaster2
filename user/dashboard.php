50170db075c1e7c801d58c1f54a9fc749ab3d9c3
tixlee <55526379+tixlee@users.noreply.github.com>
2021-03-22 01:45:34 +0800

    Fix Loading Animation

--- user/dashboard.php
+++ user/dashboard.php
@@ -15,61 +15,29 @@
 <html>
    <head>
       <title>AgileMaster | Dashboard</title>
-	  
       <?php include('../navigation/head.php');?>
-       <style type="text/css">
-
-           /* Center the loader */
-            #loader {
-              position: absolute;
-              left: 50%;
-              top: 50%;
-              z-index: 1;
-              width: 120px;
-              height: 120px;
-              margin: -76px 0 0 -76px;
-              border: 16px solid #f3f3f3;
-              border-radius: 50%;
-              border-top: 16px solid #9a1b25;
-              -webkit-animation: spin 2s linear infinite;
-              animation: spin 2s linear infinite;
-            }
-
-            @-webkit-keyframes spin {
-              0% { -webkit-transform: rotate(0deg); }
-              100% { -webkit-transform: rotate(360deg); }
-            }
-
-            @keyframes spin {
-              0% { transform: rotate(0deg); }
-              100% { transform: rotate(360deg); }
-            }
-
-            /* Add animation to "page content" */
-            .animate-bottom {
-              position: relative;
-              -webkit-animation-name: animatebottom;
-              -webkit-animation-duration: 1s;
-              animation-name: animatebottom;
-              animation-duration: 1s
-            }
-
-            @-webkit-keyframes animatebottom {
-              from { bottom:-100px; opacity:0 } 
-              to { bottom:0px; opacity:1 }
-            }
-
-            @keyframes animatebottom { 
-              from{ bottom:-100px; opacity:0 } 
-              to{ bottom:0; opacity:1 }
-            }
-
-       </style>
+<style>
+.se-pre-con {
+	position: fixed;
+	left: 0px;
+	top: 0px;
+	width: 100%;
+	height: 100%;
+	z-index: 9999;
+	background: url(../resources/images/Preloader_3.gif) center no-repeat #fff;
+}
+</style>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.2/jquery.min.js"></script>
    </head>
-   <body class="hold-transition sidebar-mini layout-fixed" onload="myFunction()" style="margin:0;">
-      <div id="loader"></div>
-	  <!-- display: block, if display: none, the bug chart alignment will not be aligned -->
-      <div class="wrapper animate-bottom" style="display:none;" id="myDiv" >
+<script>
+	$(window).load(function() {
+		$(".se-pre-con").fadeOut(1500);;
+	});
+</script>
+
+   <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="se-pre-con"></div>
+      <div class="wrapper">
          <?php include('../navigation/topbar.php');?>
          <?php include('../navigation/user/user_sidebar.php');?>
          <div class="content-wrapper">
@@ -344,18 +312,7 @@
          </div>
       </div>
        
-                            <script>
-            var myVar;
 
-            function myFunction() {
-              myVar = setTimeout(showPage, 0);
-            }
-
-            function showPage() {
-              document.getElementById("loader").style.display = "none";
-              document.getElementById("myDiv").style.display = "block";
-            }
-        </script>
        
       <!-- ./wrapper -->
       <?php
