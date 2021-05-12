e0988ccbe0d498826e69f8dc89f7f6c20f177be5
bong98 <55526360+bong98@users.noreply.github.com>
2021-05-12 14:41:00 +0800

    Update insert-chat.php

--- user/chat_function/insert-chat.php
+++ user/chat_function/insert-chat.php
@@ -7,7 +7,7 @@
         $incoming_id = mysqli_real_escape_string($conn, $_POST['incoming_id']);
         $message = mysqli_real_escape_string($conn, $_POST['message']);
         if(!empty($message)){
-            $sql = mysqli_query($conn, "INSERT INTO `messages` (`incoming_msg_id`, `outgoing_msg_id`, `msg`) VALUES ('$incoming_id', '$outgoing_id', '$message')") or die();
+            $sql = mysqli_query($conn, "INSERT INTO `messages` (`incoming_msg_id`, `outgoing_msg_id`, `msg`,`read_status`) VALUES ('$incoming_id', '$outgoing_id', '$message',0)") or die();
         }
     }else{
         header("location: ../../main/login.php");
